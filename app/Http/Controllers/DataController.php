eb58b040f5313b4158b06190a52b4e9c8d48a1e6
EgorMikhailovich <egormikhailovich112@outlook.com>
2020-11-28 22:21:39 -0800

    update

--- app/Http/Controllers/DataController.php
+++ app/Http/Controllers/DataController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+
 class DataController extends Controller
 {
     public function index()
@@ -13,7 +14,6 @@ class DataController extends Controller
     }
     public function xlsxuploadingToJson()
     {
-
 
         $target_dir = "TCPDFCustomize/ResourceData/DATA/2020/";
         // $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
@@ -39,13 +39,48 @@ class DataController extends Controller
 
             if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
                 echo "The file ". "upload.xlsx". " has been uploaded.";
+
             } 
             else {
                 echo "Sorry, there was an error uploading your file.";
             }
+            // $str = "test";
+            // echo($str{0});
+            // require_once __DIR__."/phpexcel/Classes/PHPExcel.php";
+
+            // print json data 
+            // require_once __DIR__.'./Classes/PHPExcel.php';
+            // require_once '../Classes/PHPExcel/IOFactory.php';
+            require __DIR__.'/Classes/PHPExcel.php';
+            $str ="sadfsd";
+            echo $str[0];
+            $tmpfname = "uploads/upload.xlsx";
+            $excelReader = PHPExcel_IOFactory::createReaderForFile($tmpfname);
+            $excelObj = $excelReader->load($tmpfname);
+            $worksheet = $excelObj->getSheet();//
+            $lastRow = $worksheet->getHighestRow();
+          
+            $data = [];
+            for ($row = 1; $row <= $lastRow; $row++) {
+                $data[] = [
+                    'A' => $worksheet->getCell('A'.$row)->getValue(),
+                    'B' => $worksheet->getCell('B'.$row)->getValue(),
+                    'C' => $worksheet->getCell('C'.$row)->getValue()
+                ];
+            }
+          
+            echo json_encode($data);
+            $json_data = json_encode($data);
+            file_put_contents("records.txt", $json_data);
+          
+            // Recovering
+            $the_data = file_get_contents("records.txt");
+            $the_array = json_decode($the_data);
+
+            
         }
+        $str = '[{"a":"b"}]' ;
         return response()->json('[{"a":"b"}]');
-        // $str = '[{"a":"b"}]' ;
         // $data["page_flg"]="data";
         // return view('data',$data,$str);
     }
