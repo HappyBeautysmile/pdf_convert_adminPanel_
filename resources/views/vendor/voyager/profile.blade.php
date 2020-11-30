377600700f207d75301f6dc164a03f7787fafd92
DESKTOP-JJATH2I\Cosmos <egormikhailovich112@outlook.com>
2020-11-30 12:45:07 -0800

    updaete admin page

---
+++ resources/views/vendor/voyager/profile.blade.php
@@ -0,0 +1,30 @@
+@extends('voyager::master')
+
+@section('css')
+    <style>
+        .user-email {
+            font-size: .85rem;
+            margin-bottom: 1.5em;
+        }
+        .myselfProfile{
+            background-image:url(   "{{ asset('./images/bg1.png') }}");
+        };
+    </style>
+@stop
+
+@section('content')
+    <div  class = "myselfProfile" style="background-size:cover;  background-position: center center;position:absolute; top:0; left:0; width:100%; height:300px;"></div>
+    <div style="height:160px; display:block; width:100%"></div>
+    <div style="position:relative; z-index:9; text-align:center;">
+        <img src="@if( !filter_var(Auth::user()->avatar, FILTER_VALIDATE_URL)){{ Voyager::image( Auth::user()->avatar ) }}@else{{ Auth::user()->avatar }}@endif"
+             class="avatar"
+             style="border-radius:50%; width:150px; height:150px; border:5px solid #fff;"
+             alt="{{ Auth::user()->name }} avatar">
+        <h4>{{ ucwords(Auth::user()->name) }}</h4>
+        <div class="user-email text-muted">{{ ucwords(Auth::user()->email) }}</div>
+        <p>{{ Auth::user()->bio }}</p>
+        @if ($route != '')
+            <a href="{{ $route }}" class="btn btn-primary">{{ __('voyager::profile.edit') }}</a>
+        @endif
+    </div>
+@stop
