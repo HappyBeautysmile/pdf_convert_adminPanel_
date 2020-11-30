722e8213270215e03a8b6381a30c2898197531a0
DESKTOP-JJATH2I\Cosmos <egormikhailovich112@outlook.com>
2020-11-30 12:19:04 -0800

    update admin panel

--- resources/views/vendor/voyager/dashboard/sidebar.blade.php
+++ resources/views/vendor/voyager/dashboard/sidebar.blade.php
@@ -2,19 +2,7 @@
     <nav class="navbar navbar-default" role="navigation">
         <div class="side-menu-container">
             <div class="navbar-header">
-                <a class="navbar-brand" href="{{ route('voyager.dashboard') }}">
-                    <div class="logo-icon-container">
-                        <?php $admin_logo_img = Voyager::setting('admin.icon_image', ''); ?>
-                        @if($admin_logo_img == '')
-                            <img src="{{ voyager_asset('images/logo-icon-light.png') }}" alt="Logo Icon">
-                        @else
-                            <img src="{{ Voyager::image($admin_logo_img) }}" alt="Logo Icon">
-                        @endif
-                    </div>
-                    <div class="title">dashboard</div>
-                </a>
             </div><!-- .navbar-header -->
-
             <div class="panel widget center bgimage"
                  style="background-image:url({{ Voyager::image( Voyager::setting('admin.bg_image'),('bg.jpg') ) }}); background-size: cover; background-position: 0px;">
                 <div class="dimmer"></div>
