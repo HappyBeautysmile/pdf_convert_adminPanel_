44f50791d22a249ff32ad7366b4ce372caa17a82
DESKTOP-JJATH2I\Cosmos <egormikhailovich112@outlook.com>
2020-12-06 15:27:46 -0800

    update admin page

--- resources/views/vendor/voyager/dashboard/navbar.blade.php
+++ resources/views/vendor/voyager/dashboard/navbar.blade.php
@@ -35,11 +35,14 @@
         <ul class="nav navbar-nav @if (__('voyager::generic.is_rtl') == 'true') navbar-left @else navbar-right @endif">
             <li class="dropdown profile">
                 <a href="#" class="dropdown-toggle text-right" data-toggle="dropdown" role="button"
-                   aria-expanded="false"><img src="{{ $user_avatar }}" class="profile-img"> <span
+                   aria-expanded="false">
+                   <!-- <img src="{{ $user_avatar }}" class="profile-img"> -->
+                   {{ Auth::user()->name }}
+                    <span
                             class="caret"></span></a>
                 <ul class="dropdown-menu dropdown-menu-animated">
                     <li class="profile-img">
-                        <img src="{{ $user_avatar }}" class="profile-img">
+                        <!-- <img src="{{ $user_avatar }}" class="profile-img"> -->
                         <div class="profile-body">
                             <h5>{{ Auth::user()->name }}</h5>
                             <h6>{{ Auth::user()->email }}</h6>
