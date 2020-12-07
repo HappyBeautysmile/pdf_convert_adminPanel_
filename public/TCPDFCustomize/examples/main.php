10eb4c309734b8a0a517c7c6a776747767c1c017
DESKTOP-JJATH2I\Cosmos <egormikhailovich112@outlook.com>
2020-12-06 19:18:56 -0800

    update page

--- public/TCPDFCustomize/examples/main.php
+++ public/TCPDFCustomize/examples/main.php
@@ -76,12 +76,12 @@ class MYPDF extends TCPDF {
 
     // Page footer
     public function Footer() {
-        // Position at 15 mm from bottom
-        $this->SetY(-15);
-        // Set font
-        $this->SetFont('helvetica', 'I', 8);
-        // Page number
-        $this->Cell(0, 10, 'Page '.$this->getAliasNumPage().'/'.$this->getAliasNbPages(), 0, false, 'C', 0, '', 0, false, 'T', 'M');
+        // // Position at 15 mm from bottom
+        // $this->SetY(-15);
+        // // Set font
+        // $this->SetFont('helvetica', 'I', 8);
+        // // Page number
+        // $this->Cell(0, 10, 'Page '.$this->getAliasNumPage().'/'.$this->getAliasNbPages(), 0, false, 'C', 0, '', 0, false, 'T', 'M');
     }
 }
 
