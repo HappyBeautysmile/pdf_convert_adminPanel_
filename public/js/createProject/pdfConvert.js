1d57b0a4a24c787c04c05de6d4f8989c06668ed7
DESKTOP-JJATH2I\Cosmos <egormikhailovich112@outlook.com>
2021-01-05 14:46:28 -0800

    update pdfconvert.js

--- public/js/createProject/pdfConvert.js
+++ public/js/createProject/pdfConvert.js
@@ -26,22 +26,154 @@
                 }
                 htmldata[k] = trumbowygData ;
                 htmldata[k] = htmldata[k].replaceAll("{ID}", choosedData[i]["ID"]);
-                htmldata[k] = htmldata[k].replaceAll("{Last name}", choosedData[i]["Last name"]);
-                htmldata[k] = htmldata[k].replaceAll("{First name}", choosedData[i]["First name"]);
-                htmldata[k] = htmldata[k].replaceAll("{City}", choosedData[i]["City"]);
-                htmldata[k] = htmldata[k].replaceAll("{Telephone no}", choosedData[i]["Telephone no"]);
-                htmldata[k] = htmldata[k].replaceAll("{E-mail}", choosedData[i]["E-mail"]);
-                htmldata[k] = htmldata[k].replaceAll("{Birthday_date}", choosedData[i]["Birthday_date"]);
-                htmldata[k] = htmldata[k].replaceAll("{fields_01}", choosedData[i]["fields_01"]);
-                htmldata[k] = htmldata[k].replaceAll("{fields_02}", choosedData[i]["fields_02"]);
-                htmldata[k] = htmldata[k].replaceAll("{fields_03}", choosedData[i]["fields_03"]);
-                htmldata[k] = htmldata[k].replaceAll("{fields_04}", choosedData[i]["fields_04"]);
-                htmldata[k] = htmldata[k].replaceAll("{fields_05}", choosedData[i]["fields_05"]);
-                htmldata[k] = htmldata[k].replaceAll("{fields_06}", choosedData[i]["fields_06"]);
-                htmldata[k] = htmldata[k].replaceAll("{fields_07}", choosedData[i]["fields_07"]);
-                htmldata[k] = htmldata[k].replaceAll("{fields_08}", choosedData[i]["fields_08"]);
-                htmldata[k] = htmldata[k].replaceAll("{fields_09}", choosedData[i]["fields_09"]);
-                htmldata[k] = htmldata[k].replaceAll("{fields_10}", choosedData[i]["fields_10"]);
+                if(choosedData[i]["Last name"] !=null)
+                {
+                    htmldata[k] = htmldata[k].replaceAll("{Last name}", choosedData[i]["Last name"]);
+                }
+                if(choosedData[i]["Last name"] ==null)
+                {
+                    htmldata[k] = htmldata[k].replaceAll("{Last name}", "");
+                }
+
+                if(choosedData[i]["First name"] !=null)
+                {
+                    htmldata[k] = htmldata[k].replaceAll("{First name}", choosedData[i]["First name"]);
+                }
+                if(choosedData[i]["First name"] ==null)
+                {
+                    htmldata[k] = htmldata[k].replaceAll("{First name}", "");
+                }
+
+                if(choosedData[i]["City"] !=null)
+                {
+                    htmldata[k] = htmldata[k].replaceAll("{City}", choosedData[i]["City"]);
+                }
+                if(choosedData[i]["City"] ==null)
+                {
+                    htmldata[k] = htmldata[k].replaceAll("{City}", "");
+                }
+
+                if(choosedData[i]["Telephone no"] !=null)
+                {
+                    htmldata[k] = htmldata[k].replaceAll("{Telephone no}", choosedData[i]["Telephone no"]);
+                }
+                if(choosedData[i]["Telephone no"] ==null)
+                {
+                    htmldata[k] = htmldata[k].replaceAll("{Telephone no}", "");
+                }
+  
+                if(choosedData[i]["E-mail"] !=null)
+                {
+                    htmldata[k] = htmldata[k].replaceAll("{E-mail}", choosedData[i]["E-mail"]);
+                }
+                if(choosedData[i]["E-mail"] ==null)
+                {
+                    htmldata[k] = htmldata[k].replaceAll("{E-mail}", "");
+                }
+
+                if(choosedData[i]["Birthday_date"] !=null)
+                {
+                    htmldata[k] = htmldata[k].replaceAll("{Birthday_date}", choosedData[i]["Birthday_date"]);
+                }
+                if(choosedData[i]["Birthday_date"] ==null)
+                {
+                    htmldata[k] = htmldata[k].replaceAll("{Birthday_date}", "");
+                }
+
+                
+                if(choosedData[i]["fields_01"] !=null)
+                {
+                    htmldata[k] = htmldata[k].replaceAll("{fields_01}", choosedData[i]["fields_01"]);
+                }
+                if(choosedData[i]["fields_01"] ==null)
+                {
+                    htmldata[k] = htmldata[k].replaceAll("{fields_01}", "");
+                }
+
+                if(choosedData[i]["fields_02"] !=null)
+                {
+                    htmldata[k] = htmldata[k].replaceAll("{fields_02}", choosedData[i]["fields_02"]);
+                }
+                if(choosedData[i]["fields_02"] ==null)
+                {
+                    htmldata[k] = htmldata[k].replaceAll("{fields_02}", "");
+                }
+
+                if(choosedData[i]["fields_03"] !=null)
+                {
+                    htmldata[k] = htmldata[k].replaceAll("{fields_03}", choosedData[i]["fields_03"]);
+                }
+                if(choosedData[i]["fields_03"] ==null)
+                {
+                    htmldata[k] = htmldata[k].replaceAll("{fields_03}", "");
+                }
+                
+                
+                if(choosedData[i]["fields_04"] !=null)
+                {
+                    htmldata[k] = htmldata[k].replaceAll("{fields_04}", choosedData[i]["fields_04"]);
+                }
+                if(choosedData[i]["fields_04"] ==null)
+                {
+                    htmldata[k] = htmldata[k].replaceAll("{fields_04}", "");
+                }
+
+                if(choosedData[i]["fields_05"] !=null)
+                {
+                    htmldata[k] = htmldata[k].replaceAll("{fields_05}", choosedData[i]["fields_05"]);
+                }
+                if(choosedData[i]["fields_05"] ==null)
+                {
+                    htmldata[k] = htmldata[k].replaceAll("{fields_05}", "");
+                }
+
+                if(choosedData[i]["fields_06"] !=null)
+                {
+                    htmldata[k] = htmldata[k].replaceAll("{fields_06}", choosedData[i]["fields_06"]);
+                }
+                if(choosedData[i]["fields_06"] ==null)
+                {
+                    htmldata[k] = htmldata[k].replaceAll("{fields_06}", "");
+                }
+
+                if(choosedData[i]["fields_07"] !=null)
+                {
+                    htmldata[k] = htmldata[k].replaceAll("{fields_07}", choosedData[i]["fields_07"]);
+                }
+                if(choosedData[i]["fields_07"] ==null)
+                {
+                    htmldata[k] = htmldata[k].replaceAll("{fields_07}", "");
+                }
+
+
+                if(choosedData[i]["fields_08"] !=null)
+                {
+                    htmldata[k] = htmldata[k].replaceAll("{fields_08}", choosedData[i]["fields_08"]);
+                }
+                if(choosedData[i]["fields_08"] ==null)
+                {
+                    htmldata[k] = htmldata[k].replaceAll("{fields_08}", "");
+                }
+
+
+                if(choosedData[i]["fields_09"] !=null)
+                {
+                    htmldata[k] = htmldata[k].replaceAll("{fields_09}", choosedData[i]["fields_09"]);
+                }
+                if(choosedData[i]["fields_09"] ==null)
+                {
+                    htmldata[k] = htmldata[k].replaceAll("{fields_09}", "");
+                }
+
+
+                if(choosedData[i]["fields_10"] !=null)
+                {
+                    htmldata[k] = htmldata[k].replaceAll("{fields_10}", choosedData[i]["fields_10"]);
+                }
+                if(choosedData[i]["fields_10"] ==null)
+                {
+                    htmldata[k] = htmldata[k].replaceAll("{fields_10}", "");
+                }
                 convertedPdfName[k] = choosedData[i]["ID"];
                 k++;
             }
